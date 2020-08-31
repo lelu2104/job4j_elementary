bb29cd79b3088cc2b83a062ba731e643d02c92b0
lelu2104 <elena.dubrouskaya@gmail.com>
2020-08-31 21:41:39 +0300

    5.6. Простые числа [#348977]

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -2,20 +2,6 @@ package ru.job4j.loop;
 
 public class PrimeNumber {
 
-    public static boolean check(int number) {
-        boolean prime = true;
-        for (int i = 2; i <= number; i++) {
-            if (number % i == 0) {
-                prime = false;
-                break;
-            } else {
-                prime = true;
-                break;
-            }
-        }
-        return prime;
-    }
-
     public static int calc(int finish) {
         int count = 0;
         for (int number = 2; number <= finish; number++) {
