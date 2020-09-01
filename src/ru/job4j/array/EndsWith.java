cf2203c3a883f80fcfd5251fab9fa98dab236221
lelu2104 <elena.dubrouskaya@gmail.com>
2020-09-01 23:11:23 +0300

    6.4.2. Слово заканчивается на ... [#348895]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = 0; i < word.length; i++) {
+            for (int j = 0; j < post.length; j++) {
+                if (word[word.length - 1] == post[post.length - 1]) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+}
