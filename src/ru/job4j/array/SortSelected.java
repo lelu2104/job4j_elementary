6d2daf2c369957317bb3dbad78961e0082084119
lelu2104 <elena.dubrouskaya@gmail.com>
2020-09-13 20:31:35 +0300

    6.5.3. Сортировка выборкой. [#348897]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -1,5 +1,6 @@
 package ru.job4j.array;
 
+//6.5.3. Сортировка выборкой. [#348897]
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
